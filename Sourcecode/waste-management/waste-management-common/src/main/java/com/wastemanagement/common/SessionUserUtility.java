f67b3a8f3a504348a8bee9235c81325c99421b80
ruchipareek5 <ruchipareek5@gmail.com>
2019-02-12 22:55:31 +0530

    Author                 :Ruchi Pareek Description            :Committed files for login

--- Sourcecode/waste-management/waste-management-common/src/main/java/com/wastemanagement/common/SessionUserUtility.java
+++ Sourcecode/waste-management/waste-management-common/src/main/java/com/wastemanagement/common/SessionUserUtility.java
@@ -1,36 +1,36 @@
-package com.wastemanagement.common;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-
-
-@Service
-public class SessionUserUtility {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(SessionUserUtility.class);
-	
-//	@Autowired
-//	private UserIdtoApplicationIdService userIdtoApplicationIdService;
+//package com.wastemanagement.common;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service
+//public class SessionUserUtility {
 //	
+//	private static final Logger LOGGER = LoggerFactory.getLogger(SessionUserUtility.class);
 //	
-//	 public int getApplicationId(String userId) 
-//		{	LOGGER.debug("Application Id is -" + userIdtoApplicationIdService.getApplicationId(userId));
-//		 	LOGGER.debug("Request Received to get Application Id");
-//		 	LOGGER.debug("In UserIdtoApplicationIdService - getApplicationId");
-//		 	LOGGER.debug("Request Received are - 'userId': ",userId);
-//		 	LOGGER.debug("Sending Request to service");
-//			return userIdtoApplicationIdService.getApplicationId(userId);
-//		}	
-//	 
-//	 public SessionManagementDto getSessionMangementfromSession(){
-//		 	LOGGER.debug("Request Received to get Session");
-//		 	
-//		 	LOGGER.debug("Sending Request to get SessionManagementDto");
-//		 return (SessionManagementDto)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//	 }
-//		
-
-}
+////	@Autowired
+////	private UserIdtoApplicationIdService userIdtoApplicationIdService;
+////	
+////	
+////	 public int getApplicationId(String userId) 
+////		{	LOGGER.debug("Application Id is -" + userIdtoApplicationIdService.getApplicationId(userId));
+////		 	LOGGER.debug("Request Received to get Application Id");
+////		 	LOGGER.debug("In UserIdtoApplicationIdService - getApplicationId");
+////		 	LOGGER.debug("Request Received are - 'userId': ",userId);
+////		 	LOGGER.debug("Sending Request to service");
+////			return userIdtoApplicationIdService.getApplicationId(userId);
+////		}	
+////	 
+////	 public SessionManagementDto getSessionMangementfromSession(){
+////		 	LOGGER.debug("Request Received to get Session");
+////		 	
+////		 	LOGGER.debug("Sending Request to get SessionManagementDto");
+////		 return (SessionManagementDto)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+////	 }
+////		
+//
+//}
